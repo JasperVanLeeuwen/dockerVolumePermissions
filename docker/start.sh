ae63a6a1b966b67240d67768286b7a3d9277ab42
J van Leeuwen <jasper.van.leeuwen@philips.com>
2021-01-21 08:39:00 +0100

    do not rebuild docker every time but mount volume

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ash
 echo "test" > /data/output/output.txt #example output generation
-echo $(cat id.txt)
-chown $(cat id.txt ) -R /data/output/
+#echo $(cat /id/id.txt)
+chown $(cat /id/id.txt ) -R /data/output/
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "$(id -u):$(id -g)" > docker/id.txt
+echo "$(id -u):$(id -g)" > ./id/id.txt
 docker-compose up --build
 
